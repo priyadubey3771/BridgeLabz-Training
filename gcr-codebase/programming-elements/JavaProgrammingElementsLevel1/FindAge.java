ff7497826f75045edff43aa2a41dfec4a98291ae
priyadubey3771 <priyadubey@3771>
2025-12-17 16:02:57 +0530

    [Priya]: added find age program

---
+++ gcr-codebase/programming-elements/JavaProgrammingElementsLevel1/FindAge.java
@@ -0,0 +1,16 @@
+// create a FindAge class to calculate the age of Harry
+
+public class FindAge {
+
+   public static void main (String args[]) {
+	   
+   //creating variables to store birth year, current year and age
+   int birthyear = 2000;
+   int currentYear = 2024;
+   int age = currentYear - birthyear ;
+    
+   //Displaying Harry's age	
+   System.out.println("Harry's age in 2024 is" + " " +age ) ;
+      
+  }
+}
