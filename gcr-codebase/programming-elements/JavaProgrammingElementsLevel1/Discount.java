c259c3d2dd1d59aa31c5813e620ebc8de8de2391
priyadubey3771 <priyadubey@3771>
2025-12-17 19:58:40 +0530

    [Priya] : added dicounted fee program

---
+++ gcr-codebase/programming-elements/JavaProgrammingElementsLevel1/Discount.java
@@ -0,0 +1,13 @@
+//creating class Discount to calculate the discounted fees
+public class Discount {
+   public static void main (String args[]) {
+   
+   //creating variables to store fee, discount percent and discount  
+   int Fee = 125000 ;
+   int DiscountPercent = 10 ;
+   int Discount = Fee * (DiscountPercent/100) ;
+   
+   //Displaying the discount amount and discounted fees
+   System.out.println("The discount amount is INR " +Discount +" and final discounted fee is INR " +(Fee - Discount)) ;
+   }
+}
