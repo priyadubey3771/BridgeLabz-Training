c92ade5e49645eb101a0c3d50e83d9d90d4c5d5c
priyadubey3771 <priyadubey@3771>
2025-12-18 17:11:17 +0530

    [Priya] : added int operation program

---
+++ gcr-codebase/programming-elements/JavaProgramElementsLevel2/IntOperations.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+public class IntOperations {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		
+		// Creating variables and getting user input
+        System.out.print("Enter value of a: ");
+        int a = sc.nextInt();
+        System.out.print("Enter value of b: ");
+        int b = sc.nextInt();
+        System.out.print("Enter value of c: ");
+        int c = sc.nextInt();
+		
+		//computing three integer operation and assigning the value to the variables
+        int result1 = a + b * c;    
+        int result2 = a * b + c;     
+        int result3 = c + a / b;     
+        int result4 = a % b + c;    
+		
+		 // Displaying the result
+        System.out.println("The results of Int Operations are " + result1 + ", " + result2 + ", " + result3 + ", and " + result4);
+    }
+}
