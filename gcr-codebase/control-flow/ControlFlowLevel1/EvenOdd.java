8ccd82026921c21ddbfa6872615f21244ea848c4
priyadubey3771 <priyadubey@3771>
2025-12-19 02:03:16 +0530

    [Priya]: added even odd program

---
+++ gcr-codebase/control-flow/ControlFlowLevel1/EvenOdd.java
@@ -0,0 +1,26 @@
+
+   import java.util.Scanner;
+   public class EvenOdd {
+   public static void main (String[] args){
+   Scanner input = new Scanner(System.in);
+   
+   // taking number as input from the user
+   int number;
+   System.out.println("enter the number:");
+   number = input.nextInt();
+   
+   //checking if the number is natural and displaying whether the number is even or odd from 1 to that number
+      if(number>0)
+       {
+	   for(int i = 1; i<=number ;i++)
+	   {
+	    if(i%2==0)
+		System.out.println("The number " + i + " is even\n ");
+		else
+		System.out.println("The number " + i + " is odd\n ");
+		}
+		}
+	 
+	   input.close();
+	  }
+	  }
