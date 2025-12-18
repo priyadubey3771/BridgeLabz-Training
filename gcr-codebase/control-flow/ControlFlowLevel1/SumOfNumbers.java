8ac3445c6b4f81c6dddd951d1bfae8660fb58f84
priyadubey3771 <priyadubey@3771>
2025-12-19 01:52:41 +0530

    [Priya]: added sum of numbers

---
+++ gcr-codebase/control-flow/ControlFlowLevel1/SumOfNumbers.java
@@ -0,0 +1,24 @@
+
+  import java.util.Scanner;
+  class SumOfNumbers {
+  public static void main(String[] args){
+  Scanner input = new Scanner(System.in);
+  
+  // taking variable number as input from the user and declaring variable total to store total  
+   double total = 0.0;
+   double number;
+   System.out.println("enter the number:");
+   number = input.nextDouble();
+  
+  // checking if the number is 0 or not and if the number is not 0, addind it to total and displaying total
+     while(number!=0)
+    {
+     total = total + number;
+	 System.out.println("enter the number: ");
+     number = input.nextDouble();
+	 }
+	  System.out.println("The total value is " + total);
+	  
+	  input.close();
+	  }
+	  }
