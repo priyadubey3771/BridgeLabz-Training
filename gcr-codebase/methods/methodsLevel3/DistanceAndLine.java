102886d39a85c340fa6cb229811e08539079b3bb
priyadubey3771 <priyadubey@3771>
2025-12-22 21:46:27 +0530

    [Priya]: added distance and line program

---
+++ gcr-codebase/methods/methodsLevel3/DistanceAndLine.java
@@ -0,0 +1,39 @@
+
+import java.util.Scanner;
+public class DistanceAndLine {
+
+    // Method to find Euclidean distance
+     public double findDistance(double x1,double y1,double x2,double y2) 
+	 {
+        double dx= Math.pow(x2-x1,2);
+        double dy= Math.pow(y2-y1,2);
+        return Math.sqrt(dx+dy);
+    }
+
+    // Method to find slope and y-intercept
+    public double[] findLineEquation(double x1,double y1,double x2,double y2) 
+	{
+        double m= (y2-y1)/(x2-x1);
+        double b= y1-m*x1;
+		return new double[]{m, b};
+    }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+        DistanceAndLine obj = new DistanceAndLine();
+
+        System.out.print("Enter x1 y1: ");
+        double x1 = sc.nextDouble();
+        double y1 = sc.nextDouble();
+		System.out.print("Enter x2 y2: ");
+        double x2 = sc.nextDouble();
+        double y2 = sc.nextDouble();
+
+        double distance = obj.findDistance(x1, y1, x2, y2);
+        double[] line = obj.findLineEquation(x1, y1, x2, y2);
+		
+		System.out.println("\nEuclidean Distance = " + distance);
+        System.out.println("Equation of line: y = " + line[0] + "x + " + line[1]);
+		sc.close();
+     }
+   }
