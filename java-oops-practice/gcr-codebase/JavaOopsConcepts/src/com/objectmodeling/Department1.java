7b3edce9722978601c344e0f3066db36f20063e8
priyadubey3771 <priyadubey@3771>
2026-01-01 11:40:45 +0530

    [Priya]: added Department1

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/Department1.java
@@ -0,0 +1,18 @@
+package com.objectmodeling;
+
+public class Department1 {
+
+ private String departmentName;
+
+ // Constructor
+ public Department1(String departmentName) 
+ {
+     this.departmentName = departmentName;
+ }
+
+ // Display department details
+ public void displayDepartmentDetails() 
+ {
+     System.out.println("Department Name: " + departmentName);
+ }
+}
