8e76a35a053b092401e6164e5bad9e302b692d07
priyadubey3771 <priyadubey@3771>
2026-01-01 15:42:22 +0530

    [Priya]: added developer

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/assistedproblems/Developer.java
@@ -0,0 +1,20 @@
+package com.inheritance.assistedproblems;
+
+//Developer is Employee
+public class Developer extends Employee {
+
+ private String programmingLanguage;
+
+ public Developer(String name, int id, double salary, String programmingLanguage) 
+ {
+     super(name, id, salary);
+     this.programmingLanguage = programmingLanguage;
+ }
+
+ @Override
+ public void displayDetails() 
+ {
+     super.displayDetails();
+     System.out.println("Programming Language: " + programmingLanguage);
+ }
+}
