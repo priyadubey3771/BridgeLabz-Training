36c8d1cfcbf24e5c4e76f602e04e05961d50cb1f
priyadubey3771 <priyadubey@3771>
2026-01-01 17:02:43 +0530

    [Priya]: added bank account

--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hierarchicalinheritance/BankAccount.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hierarchicalinheritance/BankAccount.java
@@ -1,5 +1,28 @@
 package com.inheritance.hierarchicalinheritance;
 
+//Superclass representing a bank account
 public class BankAccount {
 
+ protected String accountNumber;
+ protected double balance;
+
+ // Constructor
+ public BankAccount(String accountNumber, double balance) 
+ {
+     this.accountNumber = accountNumber;
+     this.balance = balance;
+ }
+
+ // Method to display common account details
+ public void displayAccountDetails() 
+ {
+     System.out.println("Account Number: " + accountNumber);
+     System.out.println("Balance: " + balance);
+ }
+
+ // Method to be overridden by subclasses
+ public String displayAccountType() 
+ {
+     return "Generic Bank Account";
+ }
 }
