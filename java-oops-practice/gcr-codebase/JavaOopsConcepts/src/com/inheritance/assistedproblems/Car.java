deb1d87cab6d412c60c0aee39ce60dda3142a056
priyadubey3771 <priyadubey@3771>
2026-01-01 15:54:05 +0530

    [Priya]: added car

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/assistedproblems/Car.java
@@ -0,0 +1,20 @@
+package com.inheritance.assistedproblems;
+
+public class Car extends Vehicle {
+
+ private int seatCapacity;
+
+ public Car(int maxSpeed, String fuelType, int seatCapacity) 
+ {
+	// calling superclass constructor
+     super(maxSpeed, fuelType);   
+     this.seatCapacity = seatCapacity;
+ }
+
+ @Override
+ public void displayInfo() 
+ {
+     super.displayInfo();
+     System.out.println("Seat Capacity: " + seatCapacity);
+ }
+}
