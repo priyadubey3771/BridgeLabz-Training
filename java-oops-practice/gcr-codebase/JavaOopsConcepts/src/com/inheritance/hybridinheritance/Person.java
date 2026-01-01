c843d24026ee9ac03b9d1cf7d872d32f4693e84f
priyadubey3771 <priyadubey@3771>
2026-01-01 21:56:11 +0530

    [Priya]: added person

--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hybridinheritance/Person.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hybridinheritance/Person.java
@@ -1,5 +1,22 @@
 package com.inheritance.hybridinheritance;
 
+//Superclass
 public class Person {
 
+ protected String name;
+ protected int id;
+
+ // Constructor
+ public Person(String name, int id) 
+ {
+     this.name = name;
+     this.id = id;
+ }
+
+ //Method to display common person details
+ public void displayPersonDetails() 
+ {
+     System.out.println("Name: " + name);
+     System.out.println("ID: " + id);
+ }
 }
