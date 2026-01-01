ba143cc42833f46adfcf1c2970d3eea00fcc9f0a
priyadubey3771 <priyadubey@3771>
2026-01-01 10:13:17 +0530

    [Priya]: added book class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/Book.java
@@ -0,0 +1,20 @@
+package com.objectmodeling;
+
+public class Book {
+
+ private String title;
+ private String author;
+
+ // Parameterized constructor
+ public Book(String title, String author) 
+ {
+     this.title = title;
+     this.author = author;
+ }
+
+ // Method to display book details
+ public void displayBookDetails() {
+     System.out.println("Book Title : " + title);
+     System.out.println("Author    : " + author);
+ }
+}
