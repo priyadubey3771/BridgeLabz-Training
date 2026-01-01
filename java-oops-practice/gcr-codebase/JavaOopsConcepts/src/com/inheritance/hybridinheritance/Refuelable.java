001032ccc57df4066c45411bfc782b86e4e0a868
priyadubey3771 <priyadubey@3771>
2026-01-01 21:53:11 +0530

    [Priya]: added refuelable

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hybridinheritance/Refuelable.java
@@ -0,0 +1,10 @@
+package com.inheritance.hybridinheritance;
+
+//Interface representing refueling behavior
+public interface Refuelable {
+
+ // Abstract method to refuel a vehicle
+ void refuel();
+ 
+}
+
