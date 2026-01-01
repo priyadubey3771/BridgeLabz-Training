f285cda0fa7aa61e5a9b24e9c9cc953a90efd1fe
priyadubey3771 <priyadubey@3771>
2026-01-01 17:26:48 +0530

    [Priya]: added worker

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hybridinheritance/Worker.java
@@ -0,0 +1,9 @@
+package com.inheritance.hybridinheritance;
+
+//Interface representing work behavior for staff
+public interface Worker {
+
+ // Abstract method to perform duties
+ void performDuties();
+}
+
