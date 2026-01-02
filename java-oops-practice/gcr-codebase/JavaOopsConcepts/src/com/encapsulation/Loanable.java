888f360b428fe69358b4d00b9636b5074def8120
priyadubey3771 <priyadubey@3771>
2026-01-02 12:22:00 +0530

    [Priya]: added loanable

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation/Loanable.java
@@ -0,0 +1,9 @@
+package com.encapsulation;
+
+//Interface defining loan-related behavior
+public interface Loanable {
+
+ void applyForLoan(double amount);
+ double calculateLoanEligibility();
+ 
+}
