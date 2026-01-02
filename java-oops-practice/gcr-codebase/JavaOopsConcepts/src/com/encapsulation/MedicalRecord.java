b5b2ff23b411e2c62e0240d2da0f0ca23c576b1e
priyadubey3771 <priyadubey@3771>
2026-01-02 14:37:58 +0530

    [Priya]: added medical record

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation/MedicalRecord.java
@@ -0,0 +1,11 @@
+package com.encapsulation;
+
+//Interface defining medical record behavior
+public interface MedicalRecord {
+
+ // Add medical record details
+ void addRecord(String record);
+
+ // View medical record details
+ void viewRecords();
+}
