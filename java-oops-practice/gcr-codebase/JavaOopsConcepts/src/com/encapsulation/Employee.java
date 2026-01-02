10672642b6d60751ff72e46ec6fc948ed942f990
priyadubey3771 <priyadubey@3771>
2026-01-02 12:05:31 +0530

    [Priya]: added employee

--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation/Employee.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation/Employee.java
@@ -1,5 +1,45 @@
 package com.encapsulation;
 
-public class Employee {
+//Abstract class representing Employee
+public abstract class Employee {
 
+ // Encapsulated fields
+ private int employeeId;
+ private String name;
+ private double baseSalary;
+
+ // Constructor
+ public Employee(int employeeId, String name, double baseSalary) 
+ {
+     this.employeeId = employeeId;
+     this.name = name;
+     this.baseSalary = baseSalary;
+ }
+
+ // Getters 
+ public int getEmployeeId() 
+ {
+     return employeeId;
+ }
+
+ public String getName() 
+ {
+     return name;
+ }
+
+ protected double getBaseSalary() 
+ {
+     return baseSalary;
+ }
+
+ // Abstract method
+ public abstract double calculateSalary();
+
+ // Concrete method
+ public void displayDetails() 
+ {
+     System.out.println("Employee ID   : " + employeeId);
+     System.out.println("Name          : " + name);
+     System.out.println("Salary        : " + calculateSalary());
+ }
 }
