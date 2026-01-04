c75540c799b59137fc727470a95477312d5fca7e
priyadubey3771 <priyadubey@3771>
2026-01-04 23:14:05 +0530

    [Priya]: added student node

--- dsa-practice/gcr-codebase/JavaDsaConcepts/src/com/linkedlist/singlylinkedlist/studentRecordManagement/StudentNode.java
+++ dsa-practice/gcr-codebase/JavaDsaConcepts/src/com/linkedlist/singlylinkedlist/studentRecordManagement/StudentNode.java
@@ -1,5 +1,23 @@
 package com.linkedlist.singlylinkedlist.studentRecordManagement;
 
-public class StudentNode {
+//This class represents a single student record
 
+public class StudentNode 
+{
+
+ int rollNo;          
+ String name;         
+ int age;             
+ char grade;          
+ StudentNode next;  
+
+ // Constructor to initialize student details
+ public StudentNode(int rollNo, String name, int age, char grade) 
+ {
+     this.rollNo = rollNo;
+     this.name = name;
+     this.age = age;
+     this.grade = grade;
+     this.next = null; 
+ }
 }
