87eafff8e747875f383124fc0cebe6cc7e75d5b3
priyadubey3771 <priyadubey@3771>
2026-01-07 16:24:17 +0530

    [Priya]:added Search for target value in 2D sorted matrix

---
+++ dsa-practice/gcr-codebase/JavaDsaConcepts/src/com/binarysearch/SearchForTargetValueIn2DSortedMatrix.java
@@ -0,0 +1,50 @@
+package com.binarysearch;
+
+public class SearchForTargetValueIn2DSortedMatrix {
+  public static void main(String[] args) {
+
+    int[][] matrix = { {1, 3, 5, 7},{10, 11, 16, 20},{23, 30, 34, 60}};
+    int target = 16;
+    boolean found = searchMatrix(matrix, target);
+    System.out.println("Target found: " + found);
+    
+}
+
+  // Binary Search on 2D matrix
+  public static boolean searchMatrix(int[][] matrix, int target) {
+
+    int rows = matrix.length;
+    int cols = matrix[0].length;
+    int left = 0;
+    int right = rows * cols - 1;
+
+    while (left <= right) 
+    {
+    	int mid = left + (right - left) / 2;
+
+        // Convert 1D index to 2D indices
+        int row = mid / cols;
+        int col = mid % cols;
+        int midValue = matrix[row][col];
+
+        if(midValue == target)
+        {
+            return true;
+        } 
+        else if(midValue > target) 
+        {
+            right = mid - 1;
+        } 
+        else 
+        {
+            left = mid + 1;
+        }
+    }
+
+    return false;
+    
+	}
+}
+
+
+
