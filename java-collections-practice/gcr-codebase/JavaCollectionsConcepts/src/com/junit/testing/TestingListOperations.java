c6ae1ffa5b1d518d0ace2942d8d1b96305d69921
priyadubey3771 <priyadubey@3771>
2026-01-27 16:02:59 +0530

    [Priya]: added Testing List Operations

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcepts/src/com/junit/testing/TestingListOperations.java
@@ -0,0 +1,22 @@
+package com.junit.testing;
+
+import java.util.List;
+public class TestingListOperations {
+
+    public void addElement(List<Integer> list, int element)
+    {
+        list.add(element);
+    }
+
+    public void removeElement(List<Integer> list, int element)
+    {
+        list.remove(Integer.valueOf(element));
+    }
+
+    public int getSize(List<Integer> list)
+    {
+        return list.size();
+    }
+}
+
+
