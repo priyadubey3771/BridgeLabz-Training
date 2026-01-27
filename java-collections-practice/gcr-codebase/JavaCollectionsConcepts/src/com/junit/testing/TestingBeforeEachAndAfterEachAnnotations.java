74e37715f18d19bb56efe6b28559fdc41b5bce83
priyadubey3771 <priyadubey@3771>
2026-01-27 16:20:02 +0530

    [Priya]: added Testing Before Each And After Each Annotations

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcepts/src/com/junit/testing/TestingBeforeEachAndAfterEachAnnotations.java
@@ -0,0 +1,25 @@
+package com.junit.testing;
+
+public class TestingBeforeEachAndAfterEachAnnotations {
+	
+  private boolean connected = false;
+
+    public void connect()
+    {
+        connected = true;
+        System.out.println("Database connected");
+    }
+
+    public void disconnect()
+    {
+        connected = false;
+        System.out.println("Database disconnected");
+    }
+
+    public  boolean isConnected()
+    {
+        return connected;
+    }
+}
+
+
