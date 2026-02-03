c92c29d87fb64f8fe1356dd07bff06503caa684c
priyadubey3771 <priyadubey@3771>
2026-02-03 14:35:44 +0530

    [Priya]: added ListToJsonDriver.java

--- java-io-stream-practice/gcr-codebase/JavaIoStreamConcepts/src/com/json/practiceproblems/ListToJsonDriver.java
+++ java-io-stream-practice/gcr-codebase/JavaIoStreamConcepts/src/com/json/practiceproblems/ListToJsonDriver.java
@@ -22,11 +22,26 @@ class StudentSimple {
     private int age;
 
     public StudentSimple() {}
-    public StudentSimple(String name, int age) { this.name = name; this.age = age; }
+    public StudentSimple(String name, int age) 
+    { 
+    	this.name = name; this.age = age; 
+    }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public String getName() 
+    { 
+    	return name; 
+    }
+    public void setName(String name) 
+    { 
+    	this.name = name; 
+    }
 
-    public int getAge() { return age; }
-    public void setAge(int age) { this.age = age; }
+    public int getAge() 
+    { 
+    	return age; 
+    }
+    public void setAge(int age) 
+    { 
+    	this.age = age; 
+    }
 }
