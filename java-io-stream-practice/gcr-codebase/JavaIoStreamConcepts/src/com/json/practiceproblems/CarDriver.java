f1c94ace89b2c14f55d5e2b658003ee1e4bf8a30
priyadubey3771 <priyadubey@3771>
2026-02-03 14:35:01 +0530

    [Priya]: added CarDriver.java

--- java-io-stream-practice/gcr-codebase/JavaIoStreamConcepts/src/com/json/practiceproblems/CarDriver.java
+++ java-io-stream-practice/gcr-codebase/JavaIoStreamConcepts/src/com/json/practiceproblems/CarDriver.java
@@ -20,12 +20,30 @@ class Car {
     private String model;
     private int year;
 
-    public String getMake() { return make; }
-    public void setMake(String make) { this.make = make; }
+    public String getMake() 
+    {
+    	return make; 
+    }
+    public void setMake(String make) 
+    { 
+    	this.make = make; 
+    }
 
-    public String getModel() { return model; }
-    public void setModel(String model) { this.model = model; }
+    public String getModel() 
+    { 
+    	return model; 
+    }
+    public void setModel(String model) 
+    { 
+    	this.model = model; 
+    }
 
-    public int getYear() { return year; }
-    public void setYear(int year) { this.year = year; }
+    public int getYear() 
+    { 
+    	return year; 
+    }
+    public void setYear(int year) 
+    { 
+    	this.year = year; 
+    }
 }
