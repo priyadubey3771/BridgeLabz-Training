57a8be7095bed6fe5cf957b12be2984f6a002f8a
priyadubey3771 <priyadubey@3771>
2026-02-03 16:41:36 +0530

    [Priya]: added user actions

--- java-io-stream-practice/scenario-based-codebase/JavaIoStreamScenarioBased/src/com/eventtracker/UserActions.java
+++ java-io-stream-practice/scenario-based-codebase/JavaIoStreamScenarioBased/src/com/eventtracker/UserActions.java
@@ -3,21 +3,25 @@ package com.eventtracker;
 public class UserActions {
 
     @AuditTrail(action = "User Login")
-    public void login(String username) {
+    public void login(String username) 
+    {
         // logic for login
     }
 
     @AuditTrail(action = "File Upload")
-    public void uploadFile(String filename) {
+    public void uploadFile(String filename) 
+    {
         // logic for uploading file
     }
 
-    public void internalProcess() {
+    public void internalProcess() 
+    {
         // Not audited
     }
 
     @AuditTrail(action = "File Delete")
-    public void deleteFile(String filename) {
+    public void deleteFile(String filename) 
+    {
         // logic for deleting file
     }
 }
