250b2b2b12bd97ff17553963273ade0197a13fd9
priyadubey3771 <priyadubey@3771>
2026-02-03 16:14:06 +0530

    [Priya]: added Requires Auth

---
+++ java-io-stream-practice/scenario-based-codebase/JavaIoStreamScenarioBased/src/com/healthcheckpro/RequiresAuth.java
@@ -0,0 +1,9 @@
+package com.healthcheckpro;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RequiresAuth {
+	
+}
